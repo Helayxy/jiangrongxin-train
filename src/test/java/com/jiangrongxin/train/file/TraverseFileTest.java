ab9e14a0c209292ff8e80a1414534153211e84d9
Helayxy <1562823234@qq.com>
2020-05-21 19:21:53 +0800

    规范了类的命名

--- src/test/java/com/jiangrongxin/train/file/TraverseFileTest.java
+++ src/test/java/com/jiangrongxin/train/file/TraverseFileTest.java
@@ -6,10 +6,10 @@ import java.io.File;
  * @author Helay
  * @date 2020/5/19 19:21
  */
-public class FileDemoTest {
+public class TraverseFileTest {
     public static void main(String[] args) {
         String path = "C:\\tmp";
         File file = new File(path);
-        FileDemo.traversalFie(file);
+        TraverseFile.traverseTheFile(file);
     }
 }
